c4cd90099554801c88a83c120fe3b66b3c824b02
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2024-11-02 12:48:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-const carData = [
-    {
-        brand: "Toyota",
-        softwareVersions: ["v1.0.0", "v1.1.0", "v1.2.0"]
-    },
-    {
-        brand: "Ford",
-        softwareVersions: ["v2.0.0", "v2.1.0"]
-    },
-    {
-        brand: "BMW",
-        softwareVersions: ["v3.0.0", "v3.1.1", "v3.2.2"]
-    }
-];
+import carData from './carData.js';
 
 // Populate brand dropdown on page load
 window.onload = function() {
